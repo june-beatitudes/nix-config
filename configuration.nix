5def160b3a3440f9b71c076550ad49f5c83e3e97
Juniper Beatitudes <weirdgirl98@proton.me>
2025-07-02 20:37:26 -0700

    Got it working

--- configuration.nix
+++ configuration.nix
@@ -60,6 +60,14 @@
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = true;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      nvidiaBusId = "PCI:01:00:0";
+      intelBusId = "PCI:00:02:0";
+    };
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
