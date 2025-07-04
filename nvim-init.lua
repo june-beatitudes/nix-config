b3fafcf739bb469df578963b16dac782e5059a14
Juniper Beatitudes <weirdgirl98@proton.me>
2025-07-04 07:19:35 -0700

    Added a couple keybinds

--- nvim-init.lua
+++ nvim-init.lua
@@ -1,4 +1,8 @@
-vim.lsp.enable('clangd')
+vim.lsp.enable("clangd")
 require("nvim-tree").setup()
 vim.cmd("colorscheme onedark")
 vim.cmd("NvimTreeOpen")
+
+-- Barbar Keybinds
+map("n", "<A-<Tab>>", "<Cmd>BufferNext<CR>", opts)
+map("n", "<C-W>", "<Cmd>BufferClose<CR>", opts)
