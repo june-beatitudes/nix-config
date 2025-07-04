be412f2abc7b73e1877761ae90ed14869cdf6e5e
Juniper Beatitudes <weirdgirl98@proton.me>
2025-07-04 08:32:41 -0700

    One last keybind change, I swear

--- nvim-init.lua
+++ nvim-init.lua
@@ -4,5 +4,6 @@ vim.cmd("colorscheme onedark")
 vim.cmd("NvimTreeOpen")
 
 -- Barbar Keybinds
-vim.keymap.set("n", "<C-<Tab>>", "<Cmd>BufferNext<CR>", opts)
+vim.keymap.set("n", "<C-A>", "<Cmd>BufferPrev<CR>", opts)
+vim.keymap.set("n", "<C-D>", "<Cmd>BufferNext<CR>", opts)
 vim.keymap.set("n", "<C-X>", "<Cmd>BufferClose<CR>", opts)
