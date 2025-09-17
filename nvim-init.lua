774c8c86d4ce6fa9fc19891ff4d3a4eec77b30e3
Juniper E.R. Beatitudes <juniper@eyes-like-fire.org>
2025-09-17 10:44:05 -0500

    Tidied up part 2

--- nvim-init.lua
+++
@@ -1,9 +0,0 @@
-vim.lsp.enable("clangd")
-require("nvim-tree").setup()
-vim.cmd("colorscheme onedark")
-vim.cmd("NvimTreeOpen")
-
--- Barbar Keybinds
-vim.keymap.set("n", "<C-A>", "<Cmd>BufferPrev<CR>", opts)
-vim.keymap.set("n", "<C-D>", "<Cmd>BufferNext<CR>", opts)
-vim.keymap.set("n", "<C-X>", "<Cmd>BufferClose<CR>", opts)
